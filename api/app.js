fd760d627df9c401cd3d713362e39882901861fc
Jesse Swidler <jesse@udacity.com>
2017-04-28 15:53:47 -0700

    Add PostgreSQL backend component

---
+++ api/app.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var Promise = require('bluebird');
+
+const pg = require('pg');
+const db = new pg.Pool({
+  user: 'temp', //env var: PGUSER
+  database: 'temp', //env var: PGDATABASE
+  password: 'temp', //env var: PGPASSWORD
+  host: 'localhost', // Server hosting the postgres database
+  port: 5433, //env var: PGPORT
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+});
+
+var api = require('./routes/api');
+
+var app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function(req,res,next) {
+    req.db = db;
+    req.query = Promise.promisify(db.query, {context: db});
+    next();
+});
+
+app.use('/', api);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    err: err.stack
+  });
+});
+
+module.exports = app;
---
+++ api/routes/api.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var fs = require('fs');
+var router = express.Router();
+
+var parch = fs.readFileSync('dbs/parch.sql').toString();
+
+router.all('/tables', function(req, res, next) {
+  req.query("SELECT table_name FROM information_schema.tables WHERE table_schema='public' AND table_type='BASE TABLE';")
+    .then(result => {
+      res.json({tables: result.rows.map(row => row.table_name)});
+    }).catch(err => {
+      err.status = 500;
+      next(err);
+    });
+});
+
+router.post('/initdb', function(req, res, next) {
+  // In the future, posting the desired db might be required somehow; ie {"db":"parch"}
+  req.query("drop owned by temp cascade;")
+    .then(function(res) {
+      return req.query(parch);
+    }).then(function() {
+      res.json({ok:1});
+    }).catch(err => {
+      err.status = 500;
+      next(err);
+    });
+});
+
+router.post('/query', function(req, res, next) {
+  req.query(req.body.query)
+    .then(result => {
+      res.json(result);
+    }).catch(err => {
+      err.status = 400;
+      next(err);
+    });
+});
+
+module.exports = router;
